5564660388f74a05259bd286a6152d4b71b88e7a
Vineet <vineet.pandey42@yahoo.com>
2015-10-19 14:15:53 -0400

    Midterm files

---
+++ midterm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int main(){
+  int v1;
+  srand(time(NULL)) ;
+ v1 = rand() %  3000000;
+  cout<< v1;
+ 
+
+ 
+}
